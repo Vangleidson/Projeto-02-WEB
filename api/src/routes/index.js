7bb27bffa948e3b664775cba05e64679e4410b78
Vangleidson <85074657+Vangleidson@users.noreply.github.com>
2021-07-26 11:49:49 -0300

    index - OK

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,14 +2,14 @@
 
 const express = require('express');
 
-const router = express.Router();
+const rota = express.Router();
 
-router.get('/api', (req, res) => {
+rota.get('/api', (req, res) => {
   res.status(200).send({
     success: 'true',
-    message: 'Seja bem-vindo(a) a API Node.js + PostgreSQL + Azure!',
+    message: 'Bem-vindo(a) a API Node.js + PostgreSQL - Vangleidson Lopes',
     version: '1.0.0',
   });
 });
 
-module.exports = router;
+module.exports = rota;
