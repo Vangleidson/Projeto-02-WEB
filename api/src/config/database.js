5518193b0c1a08440d26f3f4434e5ff7f07c82b5
Vangleidson <85074657+Vangleidson@users.noreply.github.com>
2021-07-26 11:47:57 -0300

    database.js - OK

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -5,13 +5,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// ==> Conexão com a Base de Dados:
+// Conexão com o Banco de Dados:
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 pool.on('connect', () => {
-  console.log('Base de Dados conectado com sucesso!');
+  console.log('Banco de Dados conectado com sucesso!');
 });
 
 module.exports = {
